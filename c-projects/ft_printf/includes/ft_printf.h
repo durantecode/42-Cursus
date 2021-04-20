ddb40a5ce3e6b514f2961bd56f4ced5c4c4ee9f1
Luis Durante <durante@MBP-de-Luis.JOOWIN>
2021-04-20 14:14:48 +0200

    just starting, first files

---
+++ c-projects/ft_printf/includes/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurante <ldurante@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/20 13:06:34 by ldurante          #+#    #+#             */
+/*   Updated: 2021/04/20 14:13:33 by ldurante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include "../libft/libft.h"
+# include <stdio.h>
+
+int		ft_printf(const char *format, ...);
+
+#endif
