e3152cb698c1d3c818fca8a7cb343e470a383ac0
Luis Durante <ldurante@c1r10s2.42madrid.com>
2021-04-22 20:33:09 +0200

    add GNL

---
+++ c-projects/get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/22 16:52:43 by ldurante          #+#    #+#             */
+/*   Updated: 2021/04/22 16:56:40 by ldurante         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
