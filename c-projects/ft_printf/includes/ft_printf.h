940b37c64b972fe6baa63c485896f0885fb1c93f
Luis Durante <ldurante@c1r11s1.42madrid.com>
2021-05-11 20:28:52 +0200

    started structs

--- c-projects/ft_printf/includes/ft_printf.h
+++ c-projects/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 13:06:34 by ldurante          #+#    #+#             */
-/*   Updated: 2021/05/11 13:45:00 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/05/11 20:27:27 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,21 @@
 # include "../libft/libft.h"
 # include <stdarg.h>
 # include <stdio.h>
+
+typedef struct s_print
+{         
+      va_list	args;    		// arg to print out                    
+      int		width;      	// width                
+      int		preci;        	// precision              
+      int		zero;       	// zero padding               
+      int		point;      	// .     
+      int		dash;       	// -               
+      int		length;      	// total_length (return value)              
+      int		sign;       	// positive or negative number               
+      int		is_zero;      	// is number zero                 
+      int		percen;   	// %               
+      int		space;         	// space flag ' '            
+}    t_print;
 
 int		ft_printf(const char *format, ...);
 
--- c-projects/ft_printf/srcs/ft_printf.c
+++ c-projects/ft_printf/srcs/ft_printf.c
@@ -6,28 +6,35 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/20 13:49:07 by ldurante          #+#    #+#             */
-/*   Updated: 2021/05/11 17:39:18 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/05/11 20:28:31 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-int		ft_printf(const char *format, ...)
+t_print	*ft_init_tab(t_print *tab)
 {
-	va_list	args;
-	int		counter;
-	int		i;
-	char 	*arg;
-	
-	i = 0;
-	counter = ft_strlen(format);
-	va_start(args, format);
-	while (i < counter)
+	tab->width = 0;
+	tab->preci = 0;
+	tab->zero = 0;
+	tab->point = 0;
+	tab->sign = 0;
+	tab->length = 0;
+	tab->is_zero = 0;
+	tab->dash = 0;
+	tab->percen = 0;
+	tab->space = 0;
+	return (tab);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	t_print		tab;
+
+	va_start(tab.args, format);
+	ft_init_tab(&tab)
+	while (format)
 	{
-		arg = va_arg(args, char *);
-		printf("%s\n", arg);
-		i++;
+		if (*format == %)
 	}
-	va_end(args);
-	return (counter);
 }
