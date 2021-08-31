5ca349c80cd33cb90d244c5373d483d5a625b6fd
Luis Durante <durante@Mac-mini-de-Luis.local>
2021-09-01 00:58:57 +0200

    trying to color pixels

--- c-projects/so_long/includes/so_long.h
+++ c-projects/so_long/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 00:16:00 by ldurante          #+#    #+#             */
-/*   Updated: 2021/08/31 02:58:34 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/09/01 00:58:01 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,33 @@
 # include <fcntl.h>
 
 # define BUFFER_SIZE 10
+
+typedef struct s_vector
+{
+	int x;
+	int y;
+	
+}	t_vector;
+
+typedef struct s_game
+{
+	void	*mlx_ptr;
+	void	*window;
+	void	*image;
+	char	*pixels;
+	t_vector size;
+}	t_game;
+
+// typedef struct	s_image
+// {
+// 	void      *pointer;
+// 	t_vector  size;
+// 	char      *pixels;
+// 	int       bits_per_pixel;
+// 	int       line_size;
+// 	int       endian;
+// }   t_image;
+
 
 int		main(void);
 int		get_next_line(int fd, char **line);
--- c-projects/so_long/srcs/main.c
+++ c-projects/so_long/srcs/main.c
@@ -6,42 +6,41 @@
 /*   By: ldurante <ldurante@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 00:12:34 by ldurante          #+#    #+#             */
-/*   Updated: 2021/08/31 02:58:15 by ldurante         ###   ########.fr       */
+/*   Updated: 2021/09/01 00:57:50 by ldurante         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+int		close_window(int key, int *param)
+{
+	if (key == 53)
+		exit(0);
+	return (0);
+}
+
+// t_image	ft_new_sprite(void *mlx, char *path)
+// {
+// 	t_image	img;
+	
+// 	img.pointer = mlx_xpm_file_to_image(mlx, path, &img.size.x, &img.size.y);
+// 	img.pixels  = mlx_get_data_addr(img.reference, &img.bits_per_pixel, &img.line_size, &img.endian);
+// 	return (img);
+// }
+
 int	main(void)
 {
-	void	*mlx;
-	void	*mlx_win;
-	int		img;
-	int		txt;
+	t_game	mlx;
+	int		bpp;
+	int		line;
+	int		endian;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 500, 500, "TEST");
-//	img = mlx_pixel_put(mlx, mlx_win, 200, 200, 0x00FF0000);
-
-	char	*line;
-	int		fd;
-	int		ret;
-	int		y = 200;
-
-	fd = open("text.txt", O_RDONLY);
-	ret = get_next_line(fd, &line);
-	while (ret >= 0)
-	{
-		txt = mlx_string_put(mlx, mlx_win, 200, y, 0x00FFFF00, line);
-		free(line);
-		line = NULL;
-		if (ret == 0)
-			break ;
-		ret = get_next_line(fd, &line);
-		y += 20;
-	}
-	free(line);
-	close(fd);
-//	txt = mlx_string_put(mlx, mlx_win, 200, 300, 0x00FFFF00, "PRUEBA");
-	mlx_loop(mlx);
+	mlx.mlx_ptr = mlx_init();
+	mlx.window = mlx_new_window(mlx.mlx_ptr, 800, 800, "TEST");
+	mlx.image = mlx_xpm_file_to_image(mlx.mlx_ptr, "xpm/robot2.xpm", &mlx.size.x, &mlx.size.y);
+	mlx.pixels = mlx_get_data_addr(mlx.image, &bpp, &line, &endian);
+	mlx_put_image_to_window(mlx.mlx_ptr, mlx.window, mlx.image, 100, 100);
+	mlx_hook(mlx.window, 2, 0, close_window, &mlx);
+	mlx_loop(mlx.mlx_ptr);
+	return (0);
 }
